9d0f865bd1c29743502f48d3996a26a1b198490b
Reagan-zxy <1101541882@qq.com>
2023-06-05 16:57:16 +0800

    first test Github push

---
+++ 1.常量与变量/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 3; // a就是一个变量
+    a = 5;
+}
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int num = 1000;
+
+    printf("hellw world!\n");
+
+    printf("hellw world!\n");
+    printf("hellw world!\n");
+
+}
